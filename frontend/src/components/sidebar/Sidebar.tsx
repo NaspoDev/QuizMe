45680045f74d647eadb789489ce79e0ee8ef2740
Athanasios Topaltsis <a.topaltsis@gmail.com>
2024-10-29 15:21:59 -0400

    sidebar done

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -5,10 +5,35 @@ function Sidebar() {
   return (
     <div className="Sidebar">
       <div className="heading">
-        <h1 className="title">QuizMe</h1>
-        <h2 className="subtitle">A flashcard app.</h2>
+        <h1 className="title font-bold text-2xl">QuizMe</h1>
+        <h2 className="subtitle font-light text">A flashcard app.</h2>
       </div>
-      <div className="buttons"></div>
+
+      <div className="buttons">
+        <button className="sidebar-button sidebar-button-orange">
+          All Flashcards
+        </button>
+        <button className="sidebar-button sidebar-button-orange">
+          My Topics
+        </button>
+        <button className="sidebar-button sidebar-button-green">
+          Start a Quiz
+        </button>
+      </div>
+
+      {/* Crediting myself */}
+      <div className="developer-credits-container">
+        <p className="developer-credits text-sm font-light">
+          Created by{" "}
+          <a
+            href="https://naspo.dev/"
+            target="_blank"
+            className="naspo-hyperlink"
+          >
+            Naspo
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
